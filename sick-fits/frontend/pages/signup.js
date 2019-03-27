1ba87e904f781e4b44a839a3e0ff47bce5f78cbe
Jedrzej <progreacja@gmail.com>
2019-03-27 22:18:58 +0100

    Accounts, Authentication and Permissions 30%

--- sick-fits/frontend/pages/signup.js
+++ sick-fits/frontend/pages/signup.js
@@ -1,4 +1,6 @@
+
 import Signup from '../components/Signup';
+import Signin from '../components/Signin';
 import styled from 'styled-components';
 
 const Columns = styled.div`
@@ -7,11 +9,10 @@ const Columns = styled.div`
   grid-gap: 20px;
 `;
 
-const SignupPage = (props) => (
+const SignupPage = props => (
   <Columns>
     <Signup />
-    <Signup />
-    <Signup />
+    <Signin />
   </Columns>
 );
 
